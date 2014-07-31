0c34710e91b88885c01472ba588573317d7361e7
Jude Quintana <jude.quintana@gmail.com>
2014-07-31 10:47:56 -0600

    completed warmup

--- lib/gschool_enumerable.rb
+++ lib/gschool_enumerable.rb
@@ -1,3 +1,41 @@
 module GschoolEnumerable
 
+  def g_each #(&block)
+
+    for item in self
+      #block.call(item)
+      yield(item)
+    end
+
+    self
+  end
+
+  def g_select
+    arr =[]
+    self.g_each {|item| arr << item if yield(item)}
+    arr
+  end
+
+  def g_reject
+    arr = []
+    self.g_each {|item| arr << item unless yield(item)}
+    arr
+  end
+
+  def g_map
+    arr = []
+    self.g_each{|item| arr << yield(item) }
+    arr
+  end
+
+  def g_find
+    arr = []
+    self.g_each {|item| arr <<  item if yield(item)}
+    arr.first
+  end
+
+  def g_select!
+    self.g_each { |item| self.delete(item) unless yield(item)}
+  end
+
 end
